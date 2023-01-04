075ded961fe9b2f8a13c11f32dfd041837ca6278
drumfeet <drumfeet@gmail.com>
2023-01-04 20:12:44 +0800

    Apps Table

--- nextjsapp/pages/index.tsx
+++ nextjsapp/pages/index.tsx
@@ -1,14 +1,15 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "../styles/Home.module.css";
+import AppsTable from "@components/AppsTable";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-
+      <AppsTable />
     </>
-  )
+  );
 }
